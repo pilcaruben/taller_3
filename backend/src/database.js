b903bae1fb33f271f221ae7d5eea0b712b0476d9
rubenpilca70 <rubenpilca70@gmail.com>
2025-10-25 11:51:51 -0500

    refactor: conn db

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,17 +1,17 @@
 const mongoose = require("mongoose");
 
-const URI = "mongodb://127.0.0.1:27017/mean_graphql";
+const URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/mean_graphql";
 
-const connectDB = async () => {
+(async () => {
   try {
     await mongoose.connect(URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("BD conectada");
+    console.log("BD conectada correctamente");
   } catch (err) {
     console.error("Error al conectar la BD:", err);
   }
-};
+})();
 
-module.exports = { mongoose, connectDB };
+module.exports = mongoose;
